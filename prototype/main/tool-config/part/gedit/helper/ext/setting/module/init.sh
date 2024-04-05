065e543c850fc99a35a4d0a980e3714adfd94329
samwhelp <samwhelp@users.noreply.github.com>
2024-04-05 17:09:22 +0800

    fix

---
+++ prototype/main/tool-config/part/gedit/helper/ext/setting/module/init.sh
@@ -0,0 +1,24 @@
+
+
+################################################################################
+### Head: Setting / Module / Init
+##
+
+
+##
+## Load: Setting / Module / Dconf
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/system-gsettings.sh"
+
+
+##
+## Load: Setting / Module / Gedit
+##
+
+. "${THE_MAIN_EXT_SETTING_MODULE_DIR_PATH}/gedit.sh"
+
+
+##
+### Tail: Setting / Module / Init
+################################################################################
