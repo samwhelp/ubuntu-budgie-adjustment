80e848b1b7f0bcff538759d6869cdd4b1d7fca82
samwhelp <samwhelp@users.noreply.github.com>
2024-04-19 03:27:09 +0800

    fix

--- prototype/main/budgie-config/full/budgie-config-plus-theme-light/helper/ext/setting/module/gedit.sh
+++ prototype/main/budgie-config/full/budgie-config-plus-theme-light/helper/ext/setting/module/gedit.sh
@@ -221,8 +221,8 @@ gedit_settings_set_all () {
 
 
 	util_error_echo
-	util_error_echo "gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'"
-	gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+	util_error_echo "gsettings set org.gnome.gedit.preferences.editor scheme 'solarized-light'"
+	gsettings set org.gnome.gedit.preferences.editor scheme 'solarized-light'
 
 
 	util_error_echo
