279c0f23f25eaa130cfc7f84082ae5def8dcec6c
samwhelp <samwhelp@users.noreply.github.com>
2024-04-18 21:12:26 +0800

    fix

--- prototype/main/budgie-config/part/budgie-theme-light/helper/ext/setting/module/budgie-theme-main.sh
+++ prototype/main/budgie-config/part/budgie-theme-light/helper/ext/setting/module/budgie-theme-main.sh
@@ -153,8 +153,8 @@ budgie_theme_main_settings_set_all () {
 
 
 	util_error_echo
-	util_error_echo "gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-White'"
-	gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-White'
+	util_error_echo "gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'"
+	gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-Black'
 
 
 	util_error_echo
